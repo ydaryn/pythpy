024e1f7eef2ef0ad26a00b32ca7c672c8eb3f6d8
Daryn <ermekdaryn2005@gmail.com>
2024-01-21 13:35:22 +0600

    jai

--- week1.py
+++ week1.py
@@ -1,5 +1,16 @@
-
-print("Hello World")
-print ("for check")
-print ("attem")
+#example 
+#if 5 > 2:
+#  print("Five is greater than two!")
+#if 5 > 2:
+#        print("Five is greater than two!") 
         
+#example_create_variable
+#x = 5
+#y = "Hello, World!"  
+
+#ex1
+print("Hello, World!")
+
+#ex2
+if 5 > 2:
+    print("YES")
